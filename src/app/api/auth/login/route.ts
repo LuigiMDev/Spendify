703e8093976b6f217ff501ff73536b2ea78fcfb6
LuigiMDev <luigicumaru@gmail.com>
2025-04-27 21:48:18 -0300

    feat: registro e login de usuários com retorno de JWT (apenas no backend)

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { prismadb } from "../../prismaClient";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+const loginUserSchema = z.object({
+  email: z.string().email({ message: "O e-mail é inválido!" }),
+  password: z.string().nonempty("A senha é obrigatória!"),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const parsed = loginUserSchema.safeParse(body)
+
+    if(!parsed.success) {
+
+        const parsedErrors = parsed.error.errors.map(err => ({
+            message: err.message,
+            path: err.path
+        }))
+
+        return NextResponse.json({parsedErrors}, {status: 400})
+    }
+
+    const {email, password} = parsed.data
+
+    const user = await prismadb.user.findUnique({where: {email}})
+
+    if(!user || !(await bcrypt.compare(password, user.password))) {
+        return NextResponse.json({message: "Usuário ou senha iválidos!"}, {status: 401})
+    }
+
+    const JWT_SECRET = process.env.JWT_SECRET
+
+    if(!JWT_SECRET) {
+        throw new Error("JWT_SECRET não definido!")
+    }
+
+    const token = jwt.sign(user.id, JWT_SECRET)
+
+    return NextResponse.json({token}, {status: 200})
+
+  } catch {}
+}
--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,15 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prismadb } from "../../prismaClient";
-import { User } from "../models/User";
+import { z } from "zod";
+import bcrypt from "bcryptjs";
 
-const user = new User();
+
+const registerUserSchema = z
+  .object({
+    name: z.string().nonempty({message: "O nome é obrigatório"}),
+    email: z.string().email({message: "O e-mail é inválido!"}),
+    password: z.string().nonempty({message: "A senha é obrigatória!"}),
+    confirmPassword: z.string().nonempty({message: "A confirmação da senha é obrigatória!"}),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-
+  
   try {
-    const newUser = await user.register(body);
-    return NextResponse.json({newUser}, {status: 201})
+    const body = await req.json();
+    const parsed = registerUserSchema.safeParse(body);
+
+    if (!parsed.success) {
+      const parsedErrors = parsed.error.errors.map(err => (
+        {
+          message: err.message,
+          path: err.path
+        }
+      ))
+      return NextResponse.json({ error: parsedErrors }, { status: 400 });
+    }
+
+    const { name, email, password } = parsed.data;
+
+    if (await prismadb.user.findUnique({ where: { email } })) {
+      return NextResponse.json(
+        { message: "Este e-mail já está registrado no sistema!" },
+        { status: 409 }
+      );
+    }
+
+    const passwordHashed = await bcrypt.hash(password, 10);
+
+    const newUser = await prismadb.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHashed,
+      },
+    });
+
+    return NextResponse.json({ newUser }, { status: 201 });
   } catch (err) {
     console.log(err);
     NextResponse.json(
